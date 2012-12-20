b52f92de2deab589754b79cc097841e37ce58894
willisce <willisce@missouri.edu>
2012-12-19 18:33:38 -0600

    first commit: small changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121219205052) do
+ActiveRecord::Schema.define(:version => 20121220000904) do
 
   create_table "data_types", :force => true do |t|
     t.string   "name"
@@ -179,8 +179,13 @@ ActiveRecord::Schema.define(:version => 20121219205052) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "name"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
+  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
